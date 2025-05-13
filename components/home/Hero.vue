50becabb4097bfc2473a983205194c35f043e549
zamkara <zamkara@gnuweeb.org>
2025-05-13 20:31:08 +0700

    euh

--- components/home/Hero.vue
+++ components/home/Hero.vue
@@ -1,58 +1,30 @@
 <script setup>
-import heroImg from '@/assets/images/hero.svg?raw'
-import { AreaChart } from 'lucide-vue-next'
-import { GitHubIcon } from 'vue3-simple-icons'
-
-const { title, description, github } = useAppConfig()
+const { description } = useAppConfig()
 </script>
 
 <template>
   <main
-    class="grid pt-8 pb-8 lg:grid-cols-2 place-items-center md:py-12"
+    class="grid py-8 bg-gradient-to-br from-neutral-50 dark:from-neutral-900 via-neutral-50 to-amber-100 ring-1 ring-foreground/5 rounded-3xl p-6 lg:grid-cols-2 place-items-center md:py-12"
   >
-    <div>
+    <div class="w-full ps-14">
       <h1
         class="text-5xl font-bold lg:text-6xl xl:text-7xl lg:tracking-tight xl:tracking-tighter"
       >
-        {{ title }}
+        Ciutkan tautan mu
       </h1>
-      <p class="max-w-xl mt-4 text-lg text-slate-600">
+      <p class="max-w-xl mt-4 text-lg text-neutral-600">
         {{ description }}
       </p>
       <div class="flex flex-col gap-3 mt-6 sm:flex-row">
         <HomeLink
           href="/dashboard"
-          target="_blank"
-          title="Dashboard"
-          class="flex items-center justify-center gap-1"
+          class="flex rounded-full bg-amber-400 hover:bg-amber-600 text-amber-900 hover:text-amber-100 transition-all duration-150 ease-linear text-3xl w-auto px-8 h-20 object-center font-semibold p-20 items-center justify-center gap-1"
           rel="noopener"
         >
-          <AreaChart
-            class="w-5 h-5"
-          />
-          {{ $t('dashboard.title') }}
-        </HomeLink>
-        <HomeLink
-          size="lg"
-          type="outline"
-          rel="noopener"
-          :href="github"
-          title="Github"
-          class="flex items-center justify-center gap-1"
-          target="_blank"
-        >
-          <GitHubIcon
-            class="w-5 h-5"
-          />
-          GitHub Repo
+          Mulai mengerucutkan
         </HomeLink>
       </div>
     </div>
-    <div class="hidden py-6 md:block">
-      <div
-        class="w-[512px]"
-        v-html="heroImg"
-      />
-    </div>
+    <img src="@/assets/images/hero.webp" alt="Hero image" class="w-60 translate-x-40 md:flex hidden">
   </main>
 </template>
